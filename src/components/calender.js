c78bbba25e8fa54086e2f6a6deb5af6963389cf6
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-04-17 22:32:11 +0100

    Chore: save state on selection of date

--- src/components/calender.js
+++ src/components/calender.js
@@ -1,29 +1,42 @@
 import 'moment/locale/en-gb.js';
 import React, {Component} from 'react'
-import { DatePicker, DatePickerInput } from 'rc-datepicker';
+import {DatePickerInput } from 'rc-datepicker';
 import 'rc-datepicker/lib/style.css'
+import monthConverter from '../monthConverter.json'
 
 class Calender extends Component {
   constructor(props){
     super(props)
+    this.state ={
+      selectedDate: '2020-04-16',
+      datePickerInputDate: null,
+      datePickerInputDate2: null,
+      showInput: true,
+    }
     this.onChange = this.onChange.bind(this)
   }
-  onChange = (jsDate, dateString) => {
+  onChange = async ( dateString, e) => {
+    const fullDate = '' + dateString
+    const date = fullDate[8] + fullDate[9]
+    const month = monthConverter.Months[fullDate[4]+fullDate[5]+fullDate[6]]
+    const Year = fullDate[11] + fullDate[12] + fullDate[13] + fullDate[14]
+    await this.setState({ datePickerInputDate: date + '-' + month + '-' + Year })
+    console.log(this.state.datePickerInputDate)
   }
   
   render() {
-    const date = '2020-04-16' 
     return(
-      <div> 
-    <DatePickerInput
-      onChange={this.onChange}
-      value={date}
-      className='my-custom-datepicker-component'
-      // {...anyReactInputProps}
-    />
-    <div data-test='calenderComponent'>
-    <DatePicker onChange={this.onChange} value={date}/>
-    </div>
+      <div>
+        <div data-test='calenderComponent'>
+          <DatePickerInput
+            onChange={(dateString) => { 
+              this.onChange(dateString)
+            }}
+            value={this.state.selectedDate}
+            className='my-custom-datepicker-component'
+            // {...anyReactInputProps}
+          />
+        </div>
       </div>
     )
   }
