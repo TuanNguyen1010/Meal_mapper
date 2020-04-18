4ec4ba1bb563d8f08b1ca06bec1b0544c7190e45
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-04-18 23:08:01 +0100

    Style: CSS for recipes component

--- src/components/recipes.js
+++ src/components/recipes.js
@@ -4,14 +4,19 @@ import React from 'react'
 const Recipe = ({title, calories, image, ingredients}) => {
 
   return(
-  <div> 
-    <h1> {title} </h1>
-    <img src={image} alt="" /> 
-    <h2> {calories}</h2>
-  <h2> {ingredients} </h2>
-    hello
-
-  </div>)
+  <div className='recipe-component'> 
+    <div className='recipe-container'>
+      <div className='title-container'>
+      <h1> {title} </h1>
+      </div>
+        <div className='calories-container'>
+        <h2 className='calories'> {calories}</h2>
+    </div>
+    <h2 className='ingredients'> {ingredients} </h2>
+    <img className='image' src={image} alt="" /> 
+  </div>
+  </div>
+  )
 }
 
 export default Recipe;
