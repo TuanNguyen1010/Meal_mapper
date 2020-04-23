7bcd72c8bd6d6c30dcc75f3a853a5817656b21c9
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-04-23 14:50:03 +0100

    Chore: render based on saved recipe on DB

--- client/src/pages/SelectedDate.js
+++ client/src/pages/SelectedDate.js
@@ -5,23 +5,51 @@ import axios from 'axios'
 class Date extends Component {
     constructor(props){
       super(props)
+      this.searchDB()
       this.state = {
         searchData: null,
         query: "chicken",
         recipes: null,
-        avail: false
+        avail: false,
+        existingRecipe: false,
+        savedRecipeTitle: '',
+        savedRecipeIngredients: [],
+        savedRecipeCalories: 0,
+        savedRecipeImage: ''
       }
     }
 
-  searchDB = async (e) => {
-    await axios.get('http://localhost:8000/recipe/findByDate/' + this.props.selectedDate,)
-    .then (res =>  console.log(res.data))
-  } 
+    searchDB = async () => {
+      await axios.get('http://localhost:8000/recipe/findByDate/' + this.props.selectedDate,)
+      .then (res => {
+        if (res.data){
+        this.setState({existingRecipe: true})
+        this.setState({savedRecipeTitle: res.data.title})
+        this.setState({savedRecipeIngredients: res.data.ingredients})
+        this.setState({savedRecipeCalories: res.data.calories})
+        this.setState({savedRecipeImage: res.data.image})
+        }
+      })
+    }
 
   render() {
+    const contents = this.state.existingRecipe ? (
+    <div> 
+      Saved recipe for {this.props.selectedDate}
+      <h1>{this.state.savedRecipeTitle } </h1>
+      <h3> {this.state.savedRecipeCalories}</h3>
+      <img src={this.state.savedRecipeImage} alt=""/> 
+      <h3> {this.state.savedRecipeIngredients.map((ingredients)=> (
+        <div> {ingredients}</div>
+      ))}</h3>
+    
+    </div>
+    ) : (
+      <RecipeSearcher selectedDate ={this.props.selectedDate}/>
+    )
     return(
       <div> 
-        <RecipeSearcher selectedDate ={this.props.selectedDate}/>
+        {contents}
       </div>
     ) 
   }
