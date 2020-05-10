22d1ae48504ccfa19f1703c063e8e9c884707e0c
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-05-10 19:50:15 +0100

    Refactor: recipeSearch functions

--- client/src/components/recipesSearchResult.js
+++ client/src/components/recipesSearchResult.js
@@ -4,50 +4,54 @@ import axios from 'axios'
 
 
 class RecipeSearchResult extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      recipeData: {
+        date: this.props.selectedDate,
+        recipe: [
+          {title: this.props.title,
+          calories: this.props.calories,
+          ingredients: this.props.ingredients,
+          image: this.props.image,
+          healthLabel: this.props.healthLabels,
+          url: this.props.url
+          }]
+      }
+    }
+  }
+
+  addToAdditionalDB = async() => {
+    await axios.put('/api/', this.state.recipeData)
+    .then( async () => {
+      await this.props.searchDB()
+      this.props.resetState()
+      this.props.searchAllRecipeForDate() 
+    })
+  }
+
+  addToDB = async() => {
+   await axios.post('/api/', this.state.recipeData)
+   .then( async () => {
+    await this.props.searchDB()   
+    this.props.searchAllRecipeForDate()     
+   })
+  }
 
   saveRecipe = () => {
-    const recipeData = {
-      "date": this.props.selectedDate,
-      "recipe": [
-        {"title": this.props.title,
-        "calories": this.props.calories,
-        "ingredients": this.props.ingredients,
-        "image": this.props.image,
-        'healthLabel': this.props.healthLabels,
-        'url': this.props.url
-        }]
-    }
-
-    const addToAdditionalDB = async() => {
-      await axios.put('/api/', recipeData)
-      .then( async () => {
-        await this.props.searchDB()
-        this.props.resetState()
-        this.props.searchAllRecipeForDate() 
-      })
-    }
-
-    const addToDB = async() => {
-     await axios.post('/api/', recipeData)
-     .then( async () => {
-      await this.props.searchDB()   
-      this.props.searchAllRecipeForDate()     
-     })
-    }
-
     confirmAlert({
       title: `Save this ${this.props.title} recipe to this ${this.props.selectedDate}`,
       message: 'Are you sure to do this.',
       buttons: [
         {
-          label: 'Yes',
+          label: "Yes",
           onClick: async () => {
             await axios.get('/api/' + this.props.selectedDate)
             .then(res =>{
               if (res.data){
-              addToAdditionalDB()
+              this.addToAdditionalDB()
             } else {
-            addToDB()
+            this.addToDB()
             }
             })
         }},
@@ -63,14 +67,12 @@ class RecipeSearchResult extends Component {
 
   render() {
   return(
-  <div className='recipe-search-result-component' data-test='recipe-Result-container' > 
-    <div className='recipe-search-result-container' onClick={() => {this.saveRecipe()}}>
+  <div className='recipe-search-result-component' > 
+    <div className='recipe-search-result-container' data-test='recipe-Result-container' onClick={() => {this.saveRecipe()}}>
       <div className='recipe-search-result-title'>
       <h1> {this.props.title} </h1>
       </div>
-        <div className='recipe-search-result-calories-container'>
-    </div>
-    <img className='recipe-search-result-image' data-test='recipe-Result-image' src={this.props.image} alt="" onClick={() => {this.saveRecipe()}} /> 
+    <img className='recipe-search-result-image' data-test='recipe-Result-image' src={this.props.image} alt="" /> 
     <h2 className='recipe-search-result-ingredients'> Ingredients : {this.props.ingredients.map((ingredient, key) => 
     <div key={key}> {ingredient.text} </div>
     )} </h2>
