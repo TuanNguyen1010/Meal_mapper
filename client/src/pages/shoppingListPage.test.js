0c92c7cfc94f63c00b481d163ce8254a5502de5c
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-06-02 18:02:52 +0100

    Test: Shopping page render

--- client/src/pages/shoppingListPage.test.js
+++ client/src/pages/shoppingListPage.test.js
@@ -5,4 +5,23 @@ import ShoppingListPage from './shoppingListPage'
 describe('ShoppingListPage', () => {
   let tomorrow = new Date()
   tomorrow.setDate(tomorrow.getDate() + 1);
+  
+  it('renders Shopping List container', () => {
+    const AllRecipeMock = [{
+      date: '15-06-2025', 
+      recipe: [{
+        title: 'kimchi fried rice',
+        ingredients:[{
+          text: 'kimchi'
+        }, {
+          text: 'rice'
+        }], 
+        image: 'picture of food',
+        url: 'website of the recipe' 
+      }]}]
+
+    const wrapper = shallow(<ShoppingListPage AllRecipe={AllRecipeMock}/>)
+    expect(wrapper.find("[data-test='Shopping-List-Component']").length).toBe(1)
+    expect(wrapper.find("[data-test='ingredient']").text()).toContain(AllRecipeMock[0].recipe[0].ingredients[0].text)
+  })
 })
